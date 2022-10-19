8fa03ee0616fa277c199bc38c597431ea5aadc4c
egoribragimov <egor.ibr1@gmail.com>
2022-10-20 00:02:43 +0500

    сделал HW-1-1

--- HW-1-1/HW-1-1.cpp
+++ HW-1-1/HW-1-1.cpp
@@ -6,33 +6,43 @@ using namespace std;
 int main() {
 
     float xCoordinateFirstPointLine, yCoordinateFirstPointLine, xCoordinateSecondPointLine, yCoordinateSecondPointLine,
-    xCoordinatePointOutOfLine, yCoordinatePointOutOfLine;
-    
+            xCoordinatePointOutOfLine, yCoordinatePointOutOfLine;
+
     cin >> xCoordinateFirstPointLine >> yCoordinateFirstPointLine >> xCoordinateSecondPointLine >>
-    yCoordinateSecondPointLine >> xCoordinatePointOutOfLine >> yCoordinatePointOutOfLine;
-    
+        yCoordinateSecondPointLine >> xCoordinatePointOutOfLine >> yCoordinatePointOutOfLine;
+
     double distanceFromPointToLine = fabs((xCoordinateSecondPointLine - xCoordinateFirstPointLine) *
-    (yCoordinateFirstPointLine - yCoordinatePointOutOfLine) - (xCoordinateFirstPointLine - xCoordinatePointOutOfLine) *
-    (yCoordinateSecondPointLine - yCoordinateFirstPointLine)) / sqrt((pow((xCoordinateSecondPointLine -
-    xCoordinateFirstPointLine), 2)) + pow((yCoordinateSecondPointLine - yCoordinateFirstPointLine), 2));
+                                          (yCoordinateFirstPointLine - yCoordinatePointOutOfLine) -
+                                          (xCoordinateFirstPointLine - xCoordinatePointOutOfLine) *
+                                          (yCoordinateSecondPointLine - yCoordinateFirstPointLine)) /
+                                     sqrt((pow((xCoordinateSecondPointLine -
+                                                xCoordinateFirstPointLine), 2)) +
+                                          pow((yCoordinateSecondPointLine - yCoordinateFirstPointLine), 2));
 
     float xCoordinateOfCrossing = ((xCoordinateFirstPointLine - xCoordinateSecondPointLine) *
-    (xCoordinateFirstPointLine - xCoordinateSecondPointLine) * xCoordinatePointOutOfLine +
-    (xCoordinateSecondPointLine - xCoordinateFirstPointLine) * (yCoordinateSecondPointLine - yCoordinateFirstPointLine) *
-    yCoordinatePointOutOfLine - (yCoordinateFirstPointLine * xCoordinateSecondPointLine - xCoordinateFirstPointLine *
-    yCoordinateSecondPointLine) * (yCoordinateSecondPointLine - yCoordinateFirstPointLine)) / ((yCoordinateSecondPointLine -
-    yCoordinateFirstPointLine) * (yCoordinateSecondPointLine - yCoordinateFirstPointLine) + (xCoordinateSecondPointLine -
-    xCoordinateFirstPointLine) * (xCoordinateSecondPointLine - xCoordinateFirstPointLine));
+                                   (xCoordinateFirstPointLine - xCoordinateSecondPointLine) *
+                                   xCoordinatePointOutOfLine +
+                                   (xCoordinateSecondPointLine - xCoordinateFirstPointLine) *
+                                   (yCoordinateSecondPointLine - yCoordinateFirstPointLine) *
+                                   yCoordinatePointOutOfLine -
+                                   (yCoordinateFirstPointLine * xCoordinateSecondPointLine - xCoordinateFirstPointLine *
+                                                                                             yCoordinateSecondPointLine) *
+                                   (yCoordinateSecondPointLine - yCoordinateFirstPointLine)) /
+                                  ((yCoordinateSecondPointLine -
+                                    yCoordinateFirstPointLine) *
+                                   (yCoordinateSecondPointLine - yCoordinateFirstPointLine) +
+                                   (xCoordinateSecondPointLine -
+                                    xCoordinateFirstPointLine) *
+                                   (xCoordinateSecondPointLine - xCoordinateFirstPointLine));
 
     float yCoordinateOfCrossing;
-    
+
     if (yCoordinateSecondPointLine != yCoordinateFirstPointLine) {
-    yCoordinateOfCrossing = ((xCoordinateFirstPointLine - xCoordinateSecondPointLine) * xCoordinateOfCrossing +
-    (yCoordinateSecondPointLine - yCoordinateFirstPointLine) * yCoordinatePointOutOfLine +
-    (xCoordinateSecondPointLine - xCoordinateFirstPointLine) * xCoordinatePointOutOfLine) /
-    (yCoordinateSecondPointLine - yCoordinateFirstPointLine);
-    }
-    else {
+        yCoordinateOfCrossing = ((xCoordinateFirstPointLine - xCoordinateSecondPointLine) * xCoordinateOfCrossing +
+                                 (yCoordinateSecondPointLine - yCoordinateFirstPointLine) * yCoordinatePointOutOfLine +
+                                 (xCoordinateSecondPointLine - xCoordinateFirstPointLine) * xCoordinatePointOutOfLine) /
+                                (yCoordinateSecondPointLine - yCoordinateFirstPointLine);
+    } else {
         yCoordinateOfCrossing = yCoordinateFirstPointLine;
     }
 
