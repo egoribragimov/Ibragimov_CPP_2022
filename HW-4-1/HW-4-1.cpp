f41c95a5df305950fa0b7f77b75dfc8269340a28
egoribragimov <egor.ibr1@gmail.com>
2022-10-27 01:25:19 +0500

    сделал HW-4-1

---
+++ HW-4-1/HW-4-1.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main() {
+
+    string word;
+    cin >> word;
+    int coincidences = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+
+        if (word[i] == word[word.length() - i - 1]) {
+            coincidences++;
+        }
+    }
+
+    if (coincidences == word.length()) {
+        cout << "true";
+    } else cout << "false";
+
+    return 0;
+}
