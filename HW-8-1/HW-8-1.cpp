662bbeae9485bb9aba82a0fa648c82931cf62bbd
egoribragimov <egor.ibr1@gmail.com>
2022-12-01 01:42:23 +0500

    переделал [HW-8-1]

--- HW-8-1/HW-8-1.cpp
+++ HW-8-1/HW-8-1.cpp
@@ -4,28 +4,29 @@
 using namespace std;
 
 void ReadingArray(int array[], int size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
         cin >> array[i];
-    }
 }
 
-void FromStringToInt(string inputArrayStrings[], int outputArrayInt[], int size) {
-    for (int i = 0; i < size; i++) {
-        outputArrayInt[i] = stoi(inputArrayStrings[i]);
-    }
+int *FromStringToInt(string inputArrayStrings[], int size) {
+    int *outputArray = new int[size];
+    for (int i = 0; i < size; i++)
+        outputArray[i] = stoi(inputArrayStrings[i]);
+    return outputArray;
 }
 
-string Join(int array[], string outputString, int size, char separator) {
+string Join(int array[], int size, string separator) {
+    string outputString;
     for (int i = 0; i < size; i++) {
-        outputString += std::to_string(array[i] += separator);
+        outputString += to_string(array[i]);
+        outputString += separator;
     }
     return outputString;
 }
 
-void PrintArray(int array[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << array[i] << " ";
-    }
+void PrintArray(int *pArray, int size) {
+    for (int i = 0; i < size; i++)
+        cout << pArray[i] << " ";
 }
 
 
